a078d3983f4563c6dd60fd4fff1ef6402010d316
LondoLeo <15108923+LondoLeo@users.noreply.github.com>
2023-08-28 11:30:10 +0200

    install script added

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+
+ln -s $HOME/.dotfiles/.gitconfig $HOME/.config/.gitconfig
+ln -s $HOME/.dotfiles/i3 $HOME/.config/i3
+ln -s $HOME/.dotfiles/polybar $HOME/.config/polybar
+ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/.dotfiles/.lockscreen.png $HOME/.lockscreen.png
+ln -s $HOME/.dotfiles/.wallpaper.png $HOME/.wallpaper.png
+
