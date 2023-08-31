fa9bbd24bac73df510adb54d615322965515765a
LondoLeo <15108923+LondoLeo@users.noreply.github.com>
2023-08-31 22:33:39 +0200

    removed .Xdefaults, added kitty config

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ ln -s $HOME/.dotfiles/.gitconfig $HOME/.config/.gitconfig
 ln -s $HOME/.dotfiles/i3 $HOME/.config/i3
 ln -s $HOME/.dotfiles/polybar $HOME/.config/polybar
 ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/.dotfiles/kitty $HOME/.config/kitty
 ln -s $HOME/.dotfiles/compton.conf $HOME/.config/compton.conf
 ln -s $HOME/.dotfiles/.lockscreen.png $HOME/.lockscreen.png
 ln -s $HOME/.dotfiles/.wallpaper.png $HOME/.wallpaper.png
 ln -s $HOME/.dotfiles/Scripts $HOME/Scripts
-ln -s $HOME/.dotfiles/.Xdefaults $HOME/.Xdefaults
