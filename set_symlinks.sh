70cd204d0e47969eb9d3e8a0613e8691b3a81797
LondoLeo <15108923+LondoLeo@users.noreply.github.com>
2024-03-13 17:40:07 +0100

    fixed symlinks

--- set_symlinks.sh
+++ set_symlinks.sh
@@ -2,11 +2,11 @@
 
 working_dir=$(pwd)
 
-ln -s working_dir/.lockscreen.png $HOME/.lockscreen.png
-ln -s working_dir/.wallpaper.png $HOME/.wallpaper.png
-ln -s working_dir/Scripts $HOME/Scripts
-ln -s working_dir/nvim $HOME/.config/nvim
-ln -s working_dir/i3 $HOME/.config/i3
-ln -s working_dir/kitty $HOME/.config/kitty
-ln -s working_dir/polybar $HOME/.config/polybar
-ln -s working_dir/compton.conf $HOME/.config/compton.conf
+ln -s $working_dir/.lockscreen.png $HOME/.lockscreen.png
+ln -s $working_dir/.wallpaper.png $HOME/.wallpaper.png
+ln -s $working_dir/Scripts $HOME/Scripts
+ln -s $working_dir/nvim $HOME/.config/nvim
+ln -s $working_dir/i3 $HOME/.config/i3
+ln -s $working_dir/kitty $HOME/.config/kitty
+ln -s $working_dir/polybar $HOME/.config/polybar
+ln -s $working_dir/compton.conf $HOME/.config/compton.conf
