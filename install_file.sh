0b432b97ebcac50596069cf3cc98692bd046f6e8
LondoLeo <15108923+LondoLeo@users.noreply.github.com>
2024-03-17 18:11:29 +0100

    added xautolock stuff + changed nvim clipboard to unnamedplus

--- install_file.sh
+++ install_file.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt upgrade -y
-apt install -y curl gcc g++ pavucontrol vim git feh flameshot kitty compton imagemagick lm-sensors polybar fzf zoxide ripgrep powertop
+apt install -y curl gcc g++ pavucontrol vim git feh flameshot kitty compton imagemagick lm-sensors polybar fzf zoxide ripgrep powertop xautolock
 snap install nvim --classic
 sensors-detect --auto
 powertop --auto-tune
--- nvim/lua/marcus/set.lua
+++ nvim/lua/marcus/set.lua
@@ -30,6 +30,6 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.clipboard = 'unnamed'
+vim.opt.clipboard = 'unnamedplus'
 
 vim.opt.termguicolors = true
