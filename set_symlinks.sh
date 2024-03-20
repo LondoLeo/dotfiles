ed60de082aa3b276a7afbf82c050c72b536ab628
Marcus Faulstich <marcus@srv.aivader.com>
2024-03-20 18:32:29 +0100

    replaced kitty with wezterm

--- set_symlinks.sh
+++ set_symlinks.sh
@@ -7,6 +7,6 @@ ln -s $working_dir/.wallpaper.png $HOME/.wallpaper.png
 ln -s $working_dir/Scripts $HOME/Scripts
 ln -s $working_dir/nvim $HOME/.config/nvim
 ln -s $working_dir/i3 $HOME/.config/i3
-ln -s $working_dir/kitty $HOME/.config/kitty
 ln -s $working_dir/polybar $HOME/.config/polybar
 ln -s $working_dir/compton.conf $HOME/.config/compton.conf
+ln -s $working_dir/.wezterm.lua $HOME/.wezterm.lua
